72fefcbaf85dcc7aea94964aab1a39bac1337971
travisbrigman <travis1000@gmail.com>
2021-08-13 10:48:10 -0500

    Initial Commit

---
+++ Fetch-Apprentice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fetch-Apprentice
+//
+//  Created by Travis Brigman on 8/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
