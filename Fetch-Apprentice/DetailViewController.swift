75a388c0e60143ef3cbc18926fdf5ef6be9f845f
travisbrigman <travis1000@gmail.com>
2021-08-14 06:54:33 -0500

    data loading onto view successfully

--- Fetch-Apprentice/DetailViewController.swift
+++ Fetch-Apprentice/DetailViewController.swift
@@ -12,17 +12,24 @@ class DetailViewController: UIViewController {
     var detailItem: Meal?
     let urlBase = "https://www.themealdb.com/api/json/v1/1/lookup.php?i="
     var mealRecipe = [MealRecipe]()
+    @IBOutlet weak var instructions: UILabel!
+    
+      
+    func loadDataIntoView() {
+        instructions.text = mealRecipe[0].strInstructions
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let urlString = urlBase + detailItem!.idMeal
-        
+        guard let detailItem = detailItem else { return }
+        let urlString = urlBase + detailItem.idMeal
         
         dataProvider.getMealRecipe(query: urlString) { [weak self] result in
             switch result {
             case .success(let results):
                 self?.mealRecipe = results.meals
-                
+                self?.loadDataIntoView()
 //                DispatchQueue.main.async {
 //                    self?.view.reloadData()
 //                }
