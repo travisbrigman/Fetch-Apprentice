653524dcfb50d4064af514f79fc1159cf390ca88
travisbrigman <travis1000@gmail.com>
2021-08-19 05:41:07 -0500

    fixes mealRecipe model, adds icon

--- Fetch-Apprentice/Models/MealRecipe.swift
+++ Fetch-Apprentice/Models/MealRecipe.swift
@@ -10,7 +10,7 @@ import Foundation
 struct SingleMeal: Decodable {
     let meals: [MealRecipe]
 }
-
+/*
 extension MealRecipe {
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
@@ -128,6 +128,7 @@ extension MealRecipe {
         return dict
     }
 }
+ */
 
 struct MealRecipe: Decodable {
     let idMeal: String
@@ -180,10 +181,11 @@ struct MealRecipe: Decodable {
     let strMeasure18: String?
     let strMeasure19: String?
     let strMeasure20: String?
-
+/*
     enum CodingKeys: String, CodingKey {
         case idMeal, strMeal, strDrinkAlternate, strCategory, strArea, strInstructions, strMealThumb, strTags, strYoutube
         case strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12, strIngredient13, strIngredient14, strIngredient15, strIngredient16, strIngredient17, strIngredient18, strIngredient19, strIngredient20
         case strMeasure1, strMeasure2, strMeasure3, strMeasure4, strMeasure5, strMeasure6, strMeasure7, strMeasure8, strMeasure9, strMeasure10, strMeasure11, strMeasure12, strMeasure13, strMeasure14, strMeasure15, strMeasure16, strMeasure17, strMeasure18, strMeasure19, strMeasure20
     }
+ */
 }
--- Fetch-ApprenticeTests/Fetch_ApprenticeTests.swift
+++ Fetch-ApprenticeTests/Fetch_ApprenticeTests.swift
@@ -5,10 +5,13 @@
 //  Created by Travis Brigman on 8/13/21.
 //
 
-import XCTest
 @testable import Fetch_Apprentice
+import XCTest
 
 class Fetch_ApprenticeTests: XCTestCase {
+    let tvc = TableViewController()
+    let dvc = DetailViewController()
+    let provider = Provider()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,16 +21,66 @@ class Fetch_ApprenticeTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testGetCategorysCallCompletes() throws {
+        let urlString: String = "https://www.themealdb.com/api/json/v1/1/categories.php"
+        var categories: [MealCategory] = []
+        let promise = expectation(description: "Status code: 200")
+        provider.getCategoryResults(query: urlString) { [weak self] result in
+            switch result {
+            case .success(let results):
+                categories = results.categories
+                promise.fulfill()
+            case .failure(let error):
+                XCTFail("Error: \(error.localizedDescription)")
+                print(error)
+            }
         }
+
+        wait(for: [promise], timeout: 5)
+        XCTAssertEqual(categories.count, 14)
     }
 
+    func testGetMealsInCategorysCallCompletes() throws {
+        let urlString: String = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood"
+        var meals = [Meal]()
+        let promise = expectation(description: "Status code: 200")
+        provider.getMealsInCategoryResults(query: urlString) { [weak self] result in
+            switch result {
+            case .success(let results):
+                meals = results.meals
+                promise.fulfill()
+            case .failure(let error):
+                XCTFail("🚨Error: \(error.localizedDescription)")
+                print(error)
+            }
+        }
+
+        wait(for: [promise], timeout: 10)
+        XCTAssertGreaterThan(meals.count, 0)
+    }
+
+    func testGetMealRecipeCallCompletes() throws {
+        let urlString: String = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772"
+        let promise = expectation(description: "Status code: 200")
+        provider.getMealRecipe(query: urlString) { [weak self] result in
+            switch result {
+            case .success:
+                promise.fulfill()
+            case .failure(let error):
+                XCTFail("🚨Error: \(error.localizedDescription)")
+                print(error)
+            }
+        }
+
+        wait(for: [promise], timeout: 5)
+    }
+
+    func SKIPtestGetIngredients() throws {
+        let mealRecipe = MealRecipe(idMeal: "52772", strMeal: "Teriyaki Chicken Casserole", strDrinkAlternate: nil, strCategory: "Chicken", strArea: "Japanese", strInstructions: "Preheat oven to 350\u{00b0} F. Spray a 9x13-inch baking pan with non-stick spray.\r\nCombine soy sauce, \u{00bd} cup water, brown sugar, ginger and garlic in a small saucepan and cover. Bring to a boil over medium heat. Remove lid and cook for one minute once boiling.\r\nMeanwhile, stir together the corn starch and 2 tablespoons of water in a separate dish until smooth. Once sauce is boiling, add mixture to the saucepan and stir to combine. Cook until the sauce starts to thicken then remove from heat.\r\nPlace the chicken breasts in the prepared pan. Pour one cup of the sauce over top of chicken. Place chicken in oven and bake 35 minutes or until cooked through. Remove from oven and shred chicken in the dish using two forks.\r\n*Meanwhile, steam or cook the vegetables according to package directions.\r\nAdd the cooked vegetables and rice to the casserole dish with the chicken. Add most of the remaining sauce, reserving a bit to drizzle over the top when serving. Gently toss everything together in the casserole dish until combined. Return to oven and cook 15 minutes. Remove from oven and let stand 5 minutes before serving. Drizzle each serving with remaining sauce. Enjoy!", strMealThumb: "https:\\/\\/www.themealdb.com\\/images\\/media\\/meals\\/wvpsxx1468256321.jpg", strTags: "Meat, Casserole", strYoutube: "https:\\/\\/www.youtube.com\\/watch?v=4aZr5hZXP_s", strIngredient1: "soy sauce", strIngredient2: "water", strIngredient3: "brown sugar", strIngredient4: "ground ginger", strIngredient5: "minced garlic", strIngredient6: "cornstarch", strIngredient7: "chicken breasts", strIngredient8: "stir-fry vegetables", strIngredient9: "brown rice", strIngredient10: "", strIngredient11: "", strIngredient12: "", strIngredient13: "", strIngredient14: nil, strIngredient15: "", strIngredient16: nil, strIngredient17: nil, strIngredient18: nil, strIngredient19: nil, strIngredient20: nil, strMeasure1: "3\\/4 cup", strMeasure2: "1\\/2 cup", strMeasure3: "1\\/4 cup", strMeasure4: "1\\/2 teaspoon", strMeasure5: "1\\/2 teaspoon", strMeasure6: "4 tablespoons", strMeasure7: "2", strMeasure8: "1 (12 oz.)", strMeasure9: "3 cups", strMeasure10: "", strMeasure11: "", strMeasure12: "", strMeasure13: "", strMeasure14: "", strMeasure15: "", strMeasure16: nil, strMeasure17: nil, strMeasure18: nil, strMeasure19: nil, strMeasure20: nil)
+        var ingredientNames = [String]()
+        var ingredientMeasurements = [String]()
+
+        dvc.getIngredients()
+        XCTAssertEqual(ingredientNames.count, 9)
+    }
 }
